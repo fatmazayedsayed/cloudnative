4dd2908d6b3ac06172675965e303340bb9ec2ab5
Fatma zayed <Fatma.Sayed@gbrands.com>
2023-01-01 10:48:41 +0200

    array_operations_implementation

--- Data Structure Decode/Program.cs
+++ Data Structure Decode/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Data_Structure_Decode;
+
+array_operations_implementation array_Operations_Implementation = new array_operations_implementation();
+int[] arr = new int[] { 1, 3, 5, 7 };
+Console.WriteLine("Old Array");
+Console.WriteLine(String.Join(",", arr));
+
+array_Operations_Implementation.Resize<int>(ref arr, 7);
+Console.WriteLine("New Array");
+Console.WriteLine(String.Join(",",arr ));
+
---
+++ Data Structure Decode/array-operations-implementation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structure_Decode
+{
+	public class array_operations_implementation
+	{
+		public void Resize<T>(ref T[] arrSource, int newSize)
+		{
+			//ref to send the address
+			if (newSize <= 0) return;
+			if (arrSource == null) return;
+			if (arrSource.Length == newSize) return;
+			T[] newArr = new T[newSize];
+			//copy block of bytes into new size
+			Buffer.BlockCopy(arrSource, 0, newArr, 0, Buffer.ByteLength(arrSource));
+			arrSource = newArr;
+
+		}
+	}
+}
