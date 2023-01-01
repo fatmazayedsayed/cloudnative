b4d21164a423655fb6a751ca93e9afedb94e8c43
Fatma zayed <Fatma.Sayed@gbrands.com>
2023-01-01 12:00:01 +0200

    LinkedList-implementation

---
+++ Data Structure Decode/LinkedList-implementation.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structure_Decode
+{
+	public class LinkedList
+	{
+		public LinkedListNode head = null;
+		public LinkedListNode tail = null;//pointer to the tail
+
+
+		public void InsertLast(int _data)
+		{
+			LinkedListNode newNode = new LinkedListNode(_data);
+			if (head == null)
+			{
+				//list is empty
+				head = newNode;
+				tail = newNode;
+			}
+			else
+			{
+				tail.next = newNode;
+				tail = newNode;
+			}
+
+		}
+		LinkedListIterator begin()
+		{
+			LinkedListIterator itr = new LinkedListIterator(head);
+
+			return itr;
+		}
+		public void PrintList()
+		{
+			for (LinkedListIterator itr = begin(); itr.current() != null; itr.next())
+			{
+				Console.WriteLine(itr.data());
+			}
+		}
+
+	}
+
+	public class LinkedListNode
+	{
+		public int data;
+		public LinkedListNode next;
+		public LinkedListNode(int _data)
+		{
+			data = _data;
+			next = null;
+		}
+
+	}
+
+	public class LinkedListIterator
+	{
+		LinkedListNode currentNode;
+		public LinkedListIterator()
+		{
+			currentNode = null;
+		}
+		public LinkedListIterator(LinkedListNode node)
+		{
+			currentNode = node;
+		}
+
+		public int data()
+		{
+			return currentNode.data;
+		}
+		public LinkedListIterator next()
+		{
+			currentNode = currentNode.next;
+			return this;
+		}
+		public LinkedListNode current()
+		{
+			return currentNode;
+		}
+	}
+
+}
--- Data Structure Decode/Program.cs
+++ Data Structure Decode/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Data_Structure_Decode;
+Console.WriteLine("----------------Array Operations----------------");
 
 array_operations_implementation array_Operations_Implementation = new array_operations_implementation();
 int[] arr = new int[] { 1, 3, 5, 7 };
@@ -8,9 +9,15 @@ Console.WriteLine(String.Join(",", arr));
 
 array_Operations_Implementation.Resize<int>(ref arr, 7);
 Console.WriteLine("New Array");
-Console.WriteLine(String.Join(",",arr ));
+Console.WriteLine(String.Join(",", arr));
 
 int indexOf = 40;
-int item=array_Operations_Implementation.GetAt(arr, indexOf, sizeof(int));
-Console.WriteLine("get the item at the index of {0} is {1}", indexOf,item);
+int item = array_Operations_Implementation.GetAt(arr, indexOf, sizeof(int));
+Console.WriteLine("get the item at the index of {0} is {1}", indexOf, item);
 
+Console.WriteLine("----------------Linked List----------------");
+LinkedList linkedList = new LinkedList();
+linkedList.InsertLast(1);
+linkedList.InsertLast(2);
+linkedList.InsertLast(3);
+linkedList.PrintList();
